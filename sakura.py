bf316f7fbb3ddcafe3b45f7f250c17b0158db43a
dogear <akanet.jp@gmail.com>
2024-03-01 14:16:53 +0900

    背景色変更

--- sakura.py
+++ sakura.py
@@ -4,7 +4,7 @@ index=0
 
 def btn_next():
     global index
-    root['bg']='lightblue'
+    root['bg']='aquamarine'
     index=(index+1) % len(photos)
     canvas.delete('p1')
     canvas.create_image(320,213,image=photos[index],tag='p1')
@@ -12,7 +12,7 @@ def btn_next():
 
 root=tk.Tk()
 root.geometry('700x560')
-root['bg']='lightgreen'
+root['bg']='aquamarine'
 
 canvas=tk.Canvas(root,width=640,height=426,bd=0,highlightthickness=0,relief='ridge')
 canvas.pack(pady=20)
@@ -36,6 +36,6 @@ names=[
 canvas.create_image(320,213,image=photos[index],tag='p1')
 btn=tk.Button(text='次へ',command=btn_next)
 btn.pack(ipadx=10,ipady=5)
-text_label = tk.Label(root, text="東京の桜の名所", font=("Helvetica", 16), bg="lightblue")
+text_label = tk.Label(root, text="千鳥ヶ淵(千代田区)", font=("Helvetica", 16), bg="aquamarine")
 text_label.pack()
 root.mainloop()
