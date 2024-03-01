c758e375b25903af7c395d05f33c765037b9811d
dogear <akanet.jp@gmail.com>
2024-03-01 13:47:44 +0900

    sakura6追加

--- sakura.py
+++ sakura.py
@@ -22,15 +22,17 @@ photos=[
         tk.PhotoImage(file='sakura3.png'),
         tk.PhotoImage(file='sakura4.png'),
         tk.PhotoImage(file='sakura5.png'),
+        tk.PhotoImage(file='sakura6.png'),
+        
 ]
 names=[
         "千鳥ヶ淵(千代田区)",
         "井の頭恩賜公園(武蔵野市)",
-        "中目黒目黒川(目黒区)",
+        "目黒川(目黒区)",
         "六本木ヒルズ(港区)",
-        "新宿御苑(新宿区)"
-        
-        ]
+        "新宿御苑(新宿区)",
+        "上野恩賜公園(台東区)"
+]
 canvas.create_image(320,213,image=photos[index],tag='p1')
 btn=tk.Button(text='次へ',command=btn_next)
 btn.pack(ipadx=10,ipady=5)
